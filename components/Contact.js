c2462726f9218a61c467920849f5d2385c508cd0
Tom Lee <user512@users.noreply.github.com>
2020-10-20 21:41:35 -0700

    Fix email

--- components/Contact.js
+++ components/Contact.js
@@ -4,11 +4,11 @@ import Icon from './Icon'
 const Contact = () => {
   return (
     <div id="contact" className="container">
-      <a className="button" href="mailto:me@tomlee512ee.email">Email Me</a>
+      <a className="button" href="mailto:me@tomlee.email">Email Me</a>
       <div className="icon-group">
         <Icon link="https://github.com/user512" icon="github" />
         <Icon link="https://www.linkedin.com/in/tomlee512/" icon="linkedin" />
-        <Icon link="mailto:me@tomlee512ee.email" icon="email" />
+        <Icon link="mailto:me@tomlee.email" icon="email" />
       </div>
     </div>
   )
