d0a1213fbf0f81c0d5e9238019b6745844210feb
Tom Lee <user512@users.noreply.github.com>
2020-10-23 11:23:58 -0700

    Slow down the typing for a bit so it's easier to read

--- components/Home.js
+++ components/Home.js
@@ -6,9 +6,9 @@ const Home = () => {
   return(
     <div id="home" className="container">
       <Introduction strings={[
-        "am a software engineer.",
-        "love my family and friends.",
-        "love sci-fi movies^500 and tv shows.",
+        "am a software engineer.^2000",
+        "love my family and friends.^2000",
+        "love sci-fi movies...^500 and tv shows.^2000",
       ]}/>
       <div className="icons">
         <Icon link="https://github.com/user512" icon="github" size="xl" />
