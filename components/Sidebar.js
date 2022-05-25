a4a0dad94ee303d780032e0d2724395ebf89fd1b
Tom Lee <user512@users.noreply.github.com>
2022-05-25 01:35:13 -0700

    Sidebar animation

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,29 +1,33 @@
-import { h, createRef } from 'preact'
-import { useState, useCallback } from 'preact/hooks';
+import { h, createRef } from "preact";
+import { useState, useCallback, useEffect } from "preact/hooks";
 
 export default function Sidebar(props) {
-  const [show, setShow] = useState(false)
-  const toggleShow = useCallback(() => setShow(!show), [show])
+  const ref = createRef();
+  const toggleShow = useCallback(() =>
+    ref.current.classList.toggle("nav-open")
+  );
 
   const items = [
     { href: "#home", text: "Home" },
     { href: "#about", text: "About Me" },
     { href: "#contact", text: "Contact" },
-  ]
+  ];
 
   return (
     <div className="sidebar">
-      <span className="icon menu-hamburger" onClick={ () => toggleShow() }></span>
-      { show ? <nav className="nav">
-        <span className="icon menu-close" onClick={ () => toggleShow() }></span>
+      <span className="icon menu-hamburger" onClick={toggleShow}></span>
+      <nav className="nav" ref={ref}>
+        <span className="icon menu-close" onClick={toggleShow}></span>
         <ul>
-          { items.map( ({href, text}) => (
+          {items.map(({ href, text }) => (
             <li>
-              <a href={ href } onClick={ () => toggleShow() }>{ text }</a>
+              <a href={href} onClick={toggleShow}>
+                {text}
+              </a>
             </li>
-          )) }
+          ))}
         </ul>
-      </nav> : null }
+      </nav>
     </div>
-  )
+  );
 }
